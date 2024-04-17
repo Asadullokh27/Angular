08e7379020814537edec2e6f7b6cf823bf688348
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-04-17 17:34:30 +0500

    Routes added

--- FrontAndBack_Material/FrontAndBack_Material/src/app/app-routing.module.ts
+++ FrontAndBack_Material/FrontAndBack_Material/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GetallComponent } from './components/getall/getall.component';
+import { CreateComponent } from './components/create/create.component';
+import { GetbyidComponent } from './components/getbyid/getbyid.component';
+import { UpdateComponent } from './components/update/update.component';
+import { DeleteComponent } from './components/delete/delete.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:GetallComponent},
+  {path:"getall", component:GetallComponent},
+  {path:"create", component:CreateComponent},
+  {path:"getbyid", component:GetbyidComponent},
+  {path:"update", component:UpdateComponent},
+  {path:"delete", component:DeleteComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
