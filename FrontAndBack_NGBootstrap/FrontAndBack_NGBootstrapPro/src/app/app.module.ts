441cde9aaa8bfca0b52f3dd0a430202de000dda2
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-04-17 17:32:30 +0500

    Components added

--- FrontAndBack_NGBootstrap/FrontAndBack_NGBootstrapPro/src/app/app.module.ts
+++ FrontAndBack_NGBootstrap/FrontAndBack_NGBootstrapPro/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetallComponent } from './components/getall/getall.component';
+import { CreateComponent } from './components/create/create.component';
+import { UpdateComponent } from './components/update/update.component';
+import { DeleteComponent } from './components/delete/delete.component';
+import { GetbyidComponent } from './components/getbyid/getbyid.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetallComponent,
+    CreateComponent,
+    UpdateComponent,
+    DeleteComponent,
+    GetbyidComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
