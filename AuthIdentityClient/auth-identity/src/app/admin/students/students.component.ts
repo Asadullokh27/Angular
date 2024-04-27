227c02c17fb36bbb59cdca487dc5d3ea9eab2ca1
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-04-28 00:15:52 +0500

    To be continued

--- AuthIdentityClient/auth-identity/src/app/admin/students/students.component.ts
+++ AuthIdentityClient/auth-identity/src/app/admin/students/students.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-students',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './students.component.css'
 })
 export class StudentsComponent {
+  @Input() myText!: string;
 
+  @Output() newItemEvent = new EventEmitter<string>();
+
+  addNewItem(value: string) {
+    this.newItemEvent.emit(value);
+  }
 }
